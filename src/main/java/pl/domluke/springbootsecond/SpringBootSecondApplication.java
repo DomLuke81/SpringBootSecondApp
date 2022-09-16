620ae693005336482a2085950f3d1ef0d3a2e181
DomLuke81 <d.kochanski.onoszko@gmail.com>
2022-09-16 15:11:19 +0200

    Second App in Spring Boot - starting

---
+++ src/main/java/pl/domluke/springbootsecond/SpringBootSecondApplication.java
@@ -0,0 +1,13 @@
+package pl.domluke.springbootsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecondApplication.class, args);
+    }
+
+}
